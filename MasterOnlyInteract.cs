4681a9b458ea586d297a31121d5d1e4a76d42166
Daniel <daniel.mazur@bestpractice.org.nz>
2021-01-11 10:50:15 +1300

    Create MasterOnlyInteract.cs
    Add interact check for master to call event

---
+++ MasterOnlyInteract.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class MasterOnlyInteract : UdonSharpBehaviour
+{
+
+    public string EventName;
+    public UdonBehaviour target;
+
+    void Start()
+    {
+        
+    }
+
+    public override void Interact()
+    {
+        if (Networking.IsMaster)
+        {
+            target.SendCustomEvent(EventName);
+        }
+    }
+
+}
