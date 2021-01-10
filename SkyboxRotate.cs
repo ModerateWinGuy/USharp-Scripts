71a449f8f412d71697283af510ebc9166384d51e
Daniel <daniel.mazur@bestpractice.org.nz>
2021-01-11 10:52:34 +1300

    Create SkyboxRotate.cs
    Script to slowly rotate the skybox

---
+++ SkyboxRotate.cs
@@ -0,0 +1,18 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class SkyboxRotate : UdonSharpBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * (float)0.1);
+        //To set the speed, just multiply Time.time with whatever amount you want.
+    }
+}
