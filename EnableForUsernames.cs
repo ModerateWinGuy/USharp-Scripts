7f7e7e6af9fce2f95c18700fbd222414a1dfa04a
Daniel <daniel.mazur@bestpractice.org.nz>
2021-02-10 20:28:48 +1300

    Create EnableForUsernames.cs
    Script for enabling array of gameobjects based on username

---
+++ EnableForUsernames.cs
@@ -0,0 +1,42 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class EnableForUsernames : UdonSharpBehaviour
+{
+    public GameObject[] objects;
+    public string[] UserNames;
+    void Start()
+    {
+        foreach (var name in UserNames)
+        {
+            if(Networking.LocalPlayer.displayName == name)
+            {
+                _EnableObjects();
+                return;
+            }
+        }
+
+        _DisableObjects();        
+    }
+
+    private void _EnableObjects()
+    {
+        Debug.Log($"Enabling objects for ${Networking.LocalPlayer.displayName}");
+        foreach (var obj in objects)
+        {
+            obj.SetActive(true);
+        }
+    }
+
+    private void _DisableObjects()
+    {
+        Debug.Log($"Disabling objects for ${Networking.LocalPlayer.displayName}");
+        foreach (var obj in objects)
+        {
+            obj.SetActive(false);
+        }
+    }
+}
