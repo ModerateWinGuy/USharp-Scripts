adb302b26254110792b7ec1ef7433c2df59731d4
Daniel <daniel.mazur@bestpractice.org.nz>
2021-07-01 00:46:15 +1200

    Mirror toggle script
    Disables the other object when it is enabled (disabled a different quality mirror when you turn one on so both dont run at once)

---
+++ MirrorQualityToggle.cs
@@ -0,0 +1,23 @@
+﻿using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Assets.Scripts
+{
+    class MirrorQualityToggle : UdonSharpBehaviour    
+    {
+        public GameObject Mirror;
+        public GameObject OtherObject;
+
+
+        public void Toggle()
+        {
+            Mirror.SetActive(!Mirror.activeSelf);
+
+            OtherObject.SetActive(false);
+        }
+
+    }
+}
