fd903b35fe683f9f271d34d3ec0f4946ffc2ebb2
Daniel <daniel.mazur@bestpractice.org.nz>
2021-09-17 15:42:04 +1200

    script to disable objects when leaving a trigger area

---
+++ LeaveDisable.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class LeaveDisable : UdonSharpBehaviour
+{
+    public GameObject[] ObjectsToDisable;
+
+    void Start()
+    {
+        
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        if (player != Networking.LocalPlayer) return;
+        foreach (var item in ObjectsToDisable)
+        {
+            item.SetActive(false);
+        }
+    }
+}
