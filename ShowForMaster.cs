c977abb4ae03272dfc72415e088c50c5b8585aec
Daniel <daniel.mazur@bestpractice.org.nz>
2021-10-25 13:15:55 +1300

    Basic script to enable objects only for the master

---
+++ ShowForMaster.cs
@@ -0,0 +1,32 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class RunForMaster : UdonSharpBehaviour
+{
+    [Tooltip("The Objects that will be enabled for the master of the instance")]
+    public GameObject[] ObjectsToShow;
+    void Start()
+    {
+        EnableForMaster();
+    }
+
+
+    public override void OnPlayerLeft(VRC.SDKBase.VRCPlayerApi player)
+    {
+        EnableForMaster();
+    }
+
+    private void EnableForMaster()
+    {
+        if(Networking.IsMaster)
+        {
+            foreach (var item in ObjectsToShow)
+            {
+                item.SetActive(true);
+            }
+        }
+    }
+}
