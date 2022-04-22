e03e232f83666ec6bccaaf005752f5109d1c4985
Daniel <moderatewinguy@gmail.com>
2022-04-22 17:27:52 +1200

    Create TriggerTeleport.cs
    add behaviour to teleport the user when they enter a trigger

---
+++ TriggerTeleport.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+
+public class TriggerTeleport : UdonSharpBehaviour
+{
+    public GameObject targetLocation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        player.TeleportTo(targetLocation.transform.position, targetLocation.transform.rotation);
+    }
+}
